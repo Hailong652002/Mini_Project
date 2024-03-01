10a034172f06fb63d7d774fe88295a9839a392c3
Hailong652002 <longhb555@gmail.com>
2024-03-01 09:50:19 +0700

    update main, controller, model

---
+++ Model/Posts.php
@@ -0,0 +1,31 @@
+<?php 
+require_once'db_connect.php';
+class Posts{
+    public $id;
+    public $title;
+    public $content;
+    public $image;
+/*
+    public function connectDb() {
+        $connection = mysqli_connect(DB_HOST,
+            DB_USERNAME, DB_PASSWORD, DB_NAME);
+        if (!$connection) {
+            die("Không thể kết nối. Lỗi: " .mysqli_connect_error());
+        }
+        return $connection;
+    }
+
+    public function closeDb($conn) {
+        mysqli_close($conn);
+    }
+*/
+    function insert($data){
+        require_once('db_connect.php');
+        $sql = "INSERT INTO employees (code,name,mobile,email,address,password) VALUES ('".$data['code']."','".$data['name']."','".$data['mobile']."','".$data['email']."','".$data['address']."','".md5($data['password'])."')";
+        $result = $conn->query($sql);
+        return $result;
+    }
+
+    
+}
+?>
